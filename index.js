0b13c0a3b69cae26581488681c5fe5884d85f94e
kishan singh <kishan.singh@mountblue.io>
2019-12-27 18:42:22 +0530

    used put method and validate by Joi using a function

--- index.js
+++ index.js
@@ -14,20 +14,23 @@ app.get('/', (req,res) => {
     res.send("Hello kishan is here");
 })
 
-//set rought with get method
+// get method
 app.get('/student', (req,res) => {
     res.send(courses);
 })
 
+// get methode using id
+app.get('/student/:id', (req,res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send("id in not found");
+    res.send(course);
+ })
+
 //post method with Joi
 app.post('/student', (req,res) => {
-    const Schema = {
-        name: Joi.string().min(3).required()
-    }
-
-    const result = Joi.validate(req.body, Schema);
-    if(result.error){
-        res.status(400).send(result.error.details[0].message)
+    const { error } = validateCourse(req.body)
+    if(error){
+        res.status(400).send(error.details[0].message)
         return;
     }
 
@@ -39,28 +42,27 @@ app.post('/student', (req,res) => {
     res.send(course);
 });
 
+//put method
+app.put('/student/:id', (req,res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    
+    const {error} = validateCourse(req.body);
+    if(error){
+        res.status(400).send(error.details[0].message)
+        return;
+    }
 
+    course.name = req.body.name;
+    res.send(courses);
+});
 
+function validateCourse(course){
+    const Schema = {
+        name: Joi.string().min(3).required()
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.get('/student/:id', (req,res) => {
-   const course = courses.find(c => c.id === parseInt(req.params.id));
-   if(!course) res.status(404).send("id in not found");
-   res.send(course);
-})
+    return Joi.validate(course, Schema);
+}
 
 const port = process.env.PORT || 4000;
 
